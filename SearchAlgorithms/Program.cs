750bc44ca6ab3a259195cb673daffedbaa71b48f
1SaiVaruna <sai030804@gmail.com>
2024-10-03 12:38:21 +0100

    Added functionality to generate and print a list of 10 random double values.

--- SearchAlgorithms/Program.cs
+++ SearchAlgorithms/Program.cs
@@ -2,9 +2,37 @@
 {
     internal class Program
     {
+
+        static Random randomGenerator = new Random();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<double> listOfDoubles = new List<double>();
+
+            PopulateListWithRandomDoubles(ref listOfDoubles, 10);
+            PrintList(listOfDoubles);
+
+            Console.ReadKey();
+        }
+
+        static void PopulateListWithRandomDoubles(ref List<double> list, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                double twoDigitDouble = Double.Parse(randomGenerator.NextDouble().ToString("0.00"));
+                list.Add(twoDigitDouble);
+            }
+        }
+
+        static void PrintList(List<double> list)
+        {
+            Console.WriteLine("\n\nLIST PRINT:\n");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine("  " + list[i].ToString());
+            }
+
+            Console.WriteLine("\nEnd \n");
         }
     }
 }
